d2c98349d8393f953b548f668865a0dcde445420
GomdimApps <isackmota6565@gmail.com>
2024-08-08 10:56:32 -0300

    refactor(information system) I improved the code structure to make it cleaner

--- tools/bedrock/system/system.go
+++ tools/bedrock/system/system.go
@@ -19,12 +19,11 @@ const ConfigFilePath = "/etc/mineservertools/bedrock-server.conf"
 func GetServerDir() string {
 	content, err := ioutil.ReadFile(ConfigFilePath)
 	if err != nil {
-		fmt.Printf("Error reading the bedrock configuration file")
+		fmt.Println("Erro ao ler o arquivo de configuração do Bedrock")
 		return ""
 	}
 
-	lines := strings.Split(string(content), "\n")
-	for _, line := range lines {
+	for _, line := range strings.Split(string(content), "\n") {
 		if strings.HasPrefix(line, "server-dir=") {
 			return strings.Trim(strings.Split(line, "=")[1], "\"")
 		}
@@ -49,20 +48,20 @@ func DirSize(path string) (uint64, error) {
 func main() {
 	cpuPercent, err := cpu.Percent(time.Second, false)
 	if err != nil {
-		fmt.Printf("Error getting CPU percent: %v\n", err)
+		fmt.Printf("Erro ao obter a porcentagem de uso da CPU: %v\n", err)
 		return
 	}
 
 	memory, err := mem.VirtualMemory()
 	if err != nil {
-		fmt.Printf("Error getting memory usage: %v\n", err)
+		fmt.Printf("Erro ao obter o uso da memória: %v\n", err)
 		return
 	}
 
-	d, _ := disk.Usage("/")
-	totalGB := d.Total / 1024 / 1024 / 1024
-	freeGB := d.Free / 1024 / 1024 / 1024
-	usedGB := d.Used / 1024 / 1024 / 1024
+	diskUsage, _ := disk.Usage("/")
+	totalGB := diskUsage.Total / 1024 / 1024 / 1024
+	freeGB := diskUsage.Free / 1024 / 1024 / 1024
+	usedGB := diskUsage.Used / 1024 / 1024 / 1024
 
 	if memory.UsedPercent >= 85 {
 		fmt.Println("##### AVISO: a utilização da memória é superior a 85%")
@@ -70,13 +69,13 @@ func main() {
 
 	serverDir := GetServerDir()
 	if serverDir == "" {
-		fmt.Println("Error getting the server directory.")
+		fmt.Println("Erro ao obter o diretório do servidor.")
 		return
 	}
 
 	serverDirSize, err := DirSize(serverDir)
 	if err != nil {
-		fmt.Printf("Error getting server directory size: %v\n", err)
+		fmt.Printf("Erro ao obter o tamanho do diretório do servidor: %v\n", err)
 		return
 	}
 
@@ -86,7 +85,7 @@ func main() {
 	fmt.Printf("Memória livre: %v MB\n", memory.Free/1024/1024)
 	fmt.Printf("Total de Disco: %v GB\n", totalGB)
 	fmt.Printf("Disco Livre: %v GB\n", freeGB)
-	fmt.Printf("Disco Usado: %v GB (%.2f%%)\n\n", usedGB, d.UsedPercent)
+	fmt.Printf("Disco Usado: %v GB (%.2f%%)\n\n", usedGB, diskUsage.UsedPercent)
 
 	if serverDirSize < 1024*1024*1024 {
 		fmt.Printf("Tamanho do servidor: %v MB\n", serverDirSize/1024/1024)
@@ -97,10 +96,9 @@ func main() {
 	cmd := exec.Command("sh", "-c", "top -b -n 1 | grep -q \"bedrock\" && echo \"Operando\" || echo \"Inativo\"")
 	output, err := cmd.Output()
 	if err != nil {
-		fmt.Printf("Error checking server status")
+		fmt.Println("Erro ao verificar o status do servidor")
 		return
 	}
-
 	fmt.Printf("Status do servidor: %s", output)
 
 	cmd = exec.Command("sh", "-c", "ip route get 8.8.8.8 | awk '{print $7}'")
@@ -111,7 +109,7 @@ func main() {
 	}
 	fmt.Printf("\nIP do servidor: %s\n", strings.TrimSpace(string(ipOutput)))
 
-	cmd = exec.Command("sh", "-c", `output=$(lsof -i -P -n | grep bedrock | awk '/IPv4/ {print "IPv4: " $9} /IPv6/ {print "IPv6: " $9}'); if [ -z "$output" ]; then echo "Server inativo"; else echo "$output"; fi`)
+	cmd = exec.Command("sh", "-c", `output=$(lsof -i -P -n | grep bedrock | awk '/IPv4/ {print "IPv4: " $9} /IPv6/ {print "IPv6: " $9}'); if [ -z "$output" ]; then echo "Servidor inativo"; else echo "$output"; fi`)
 	portsOutput, err := cmd.Output()
 	if err != nil {
 		fmt.Printf("Erro ao obter as portas utilizadas: %v\n", err)
